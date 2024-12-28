b8f7f46a50ca560c8cf4043e025834b5ff13ffac
SriVenkat77 <venkateshwaran1112@gmail.com>
2024-12-29 00:43:31 +0530

    FRONTEND

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./App.css";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from './Pages/Auth/Login';
+import Register from './Pages/Auth/Register';
+// App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Pages/Home/Home';
+import SetAvatar from './Pages/Avatar/setAvatar';
+import Resetpassword from './Pages/Auth/forgotPassword';
+
+
+
+const App = () => {
+  return (
+    
+      <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgotPassword" element={<Resetpassword />} />
+          <Route path="/setAvatar" element={<SetAvatar />} />
+        </Routes>
+      </BrowserRouter>
+      </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Auth/forgotPassword.js
@@ -0,0 +1,237 @@
+// LoginPage.js
+import { useCallback, useEffect, useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { resetPasswordAPI } from "../../utils/ApiRequest";
+
+const Resetpassword = () => {
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  const [values, setValues] = useState({
+    email: "",
+    newPassword: "",
+    confirmPassword: ""
+  });
+
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { email, newPassword, confirmPassword } = values;
+
+    if (!email || !newPassword || !confirmPassword) {
+      toast.error("Please fill all fields", toastOptions);
+      return;
+    }
+
+    if (newPassword !== confirmPassword) {
+      toast.error("Passwords do not match", toastOptions);
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const { data } = await axios.post(resetPasswordAPI, {
+        email: email,
+        password: newPassword
+      });
+
+      if (data.success === true) {
+        navigate("/login");
+        toast.success(data.message, toastOptions);
+      } else {
+        toast.error(data.message, toastOptions);
+      }
+    } catch (error) {
+      toast.error("Something went wrong", toastOptions);
+    }
+    setLoading(false);
+  };
+
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+  }, []);
+
+  return (
+    <div style={{ position: "relative", overflow: "hidden" }}>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "#000",
+            },
+          },
+          fpsLimit: 60,
+          particles: {
+            number: {
+              value: 200,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: "#ffcc00",
+            },
+            shape: {
+              type: "circle",
+            },
+            opacity: {
+              value: 0.5,
+              random: true,
+            },
+            size: {
+              value: 3,
+              random: { enable: true, minimumValue: 1 },
+            },
+            links: {
+              enable: false,
+            },
+            move: {
+              enable: true,
+              speed: 2,
+            },
+            life: {
+              duration: {
+                sync: false,
+                value: 3,
+              },
+              count: 0,
+              delay: {
+                random: {
+                  enable: true,
+                  minimumValue: 0.5,
+                },
+                value: 1,
+              },
+            },
+          },
+          detectRetina: true,
+        }}
+        style={{
+          position: "absolute",
+          zIndex: -1,
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+      />
+      <Container
+        className="mt-5"
+        style={{ position: "relative", zIndex: "2 !important" }}
+      >
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <h1 className="text-center mt-5">
+              <AccountBalanceWalletIcon
+                sx={{ fontSize: 40, color: "white" }}
+                className="text-center"
+              />
+            </h1>
+            <h2 className="text-white text-center ">Reset Password</h2>
+            <Form>
+              <Form.Group controlId="formBasicEmail" className="mt-3">
+                <Form.Label className="text-white">Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                  onChange={handleChange}
+                  value={values.email}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicNewPassword" className="mt-3">
+                <Form.Label className="text-white">New Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="newPassword"
+                  placeholder="New Password"
+                  onChange={handleChange}
+                  value={values.newPassword}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicConfirmPassword" className="mt-3">
+                <Form.Label className="text-white">Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  onChange={handleChange}
+                  value={values.confirmPassword}
+                />
+              </Form.Group>
+
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                }}
+                className="mt-4"
+              >
+                <Button
+                  type="submit"
+                  className="text-center mt-3 btnStyle"
+                  onClick={!loading ? handleSubmit : null}
+                  disabled={loading}
+                >
+                  {loading ? "Resetting..." : "Reset Password"}
+                </Button>
+
+                <p className="mt-3" style={{ color: "#9d9494" }}>
+                  Remember your password?{" "}
+                  <Link to="/login" className="text-white lnk">
+                    Login
+                  </Link>
+                </p>
+              </div>
+            </Form>
+          </Col>
+        </Row>
+        <ToastContainer />
+      </Container>
+    </div>
+  );
+};
+
+export default Resetpassword;
